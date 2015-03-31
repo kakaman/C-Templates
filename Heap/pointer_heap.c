d77c0acc6e7eadb57fec6958eda8f862afd78b1a
vyshnav <vyshnav@msn.com>
2015-03-30 23:19:07 -0500

    Modified the mutable heap

--- Heap/pointer_heap.c
+++ Heap/pointer_heap.c
@@ -4,41 +4,27 @@
 #include <stdbool.h>
 #include <stddef.h>
 
-#include "pointer_heap.h"
+#include "heap.h"
 
 #define PARENT(x) ((x - 1) / 2)
 #define LEFT_CHILD(x) (2 * x + 1)
 #define RIGHT_CHILD(x) (2 * x + 2)
-
-typedef struct _heap_data_t
-{
-    int key;
-    int heap_location;
-    char* value;
-}heap_data_t;
-
-void node_copy(heap_t* heap, int location, void const* data)
-{
-    memcpy(heap->data + location, data, heap->data_size);
-
-    return;
-}
+#define INVALID_LOCATION -1
 
 // Initialize the heap. Returns an empty max heap with size 0.
-heap_t* heap_init(int data_size, int location_offset,
+heap_t* heap_init(int location_offset,
                   int (*compare)(void const*, void const*),
                   bool (*find)(void const*, void const*),
                   void (*print)(void const*))
 {
     heap_t* heap = malloc(sizeof(heap_t));
     heap->size = 0;
-    heap->data_size = data_size;
     heap->location_offset = location_offset;
     heap->print = print;
     heap->compare = compare;
     heap->find = find;
     heap->allocated_size = 1;
-    heap->data = malloc(data_size);
+    heap->data = malloc(sizeof(void *));
 
     return heap;
 }
@@ -66,18 +52,18 @@ void heapify_down(heap_t* heap, int location)
     int right = RIGHT_CHILD(location);
 
     void* location_ptr = heap->data[location];
-    void* min_ptr = heap->data[min];
-    void* left_ptr = heap->data[left];
+    void* min_ptr = location_ptr;
 
-    if ((left < heap->size) && (heap->compare(left_ptr, min_ptr) < 0))
+    if ((left < heap->size) && (heap->compare(heap->data[left], min_ptr) < 0))
     {
+        void* left_ptr = heap->data[left];
         min = left;
         min_ptr = left_ptr;
     }
 
-    void const* right_ptr = heap->data[right];
-    if ((right < heap->size) && (heap->compare(right_ptr, min_ptr) < 0))
+    if ((right < heap->size) && (heap->compare(heap->data[right], min_ptr) < 0))
     {
+        void* right_ptr = heap->data[right];
         min = right;
         min_ptr = right_ptr;
     }
@@ -86,10 +72,10 @@ void heapify_down(heap_t* heap, int location)
     {
         int offset = heap->location_offset;
 
-        int *min_location = (int *)(min_ptr + offset);
+        int* min_location = (int *)(min_ptr + offset);
         *min_location = location;
 
-        int *location_location = (int *)(location_ptr + offset);
+        int* location_location = (int *)(location_ptr + offset);
         *location_location = min;
 
         heap->data[location] = heap->data[min];
@@ -112,10 +98,10 @@ void heapify_up(heap_t* heap, int location)
     {
         int offset = heap->location_offset;
 
-        int *parent_location = (int *)(parent_ptr + offset);
+        int* parent_location = (int *)(parent_ptr + offset);
         *parent_location = location;
 
-        int *location_location = (int *)(location_ptr + offset);
+        int* location_location = (int *)(location_ptr + offset);
         *location_location = parent;
 
         heap->data[location] = heap->data[parent];
@@ -128,16 +114,16 @@ void heapify_up(heap_t* heap, int location)
 }
 
 // Inserts node into the max heap.
-void heap_insert(heap_t* heap, void const* data)
+void heap_insert(heap_t* heap, void* data)
 {
     if (heap->size == heap->allocated_size)
     {
-        heap->data = realloc(heap->data, (heap->allocated_size * 2) * heap->data_size);
+        heap->data = realloc(heap->data, (heap->allocated_size * 2) * sizeof(void*));
         heap->allocated_size *= 2;
     }
 
     heap->data[heap->size] = data;
-    int *location_location = (int *)(heap->data[heap->size] + heap->location_offset);
+    int *location_location = (int *)(data + heap->location_offset);
     *location_location = heap->size;
 
     heap->size++;
@@ -154,8 +140,11 @@ void* heap_remove_at(heap_t* heap, int location)
     void* last_ptr = heap->data[heap->size - 1];
     int comp_val = heap->compare(last_ptr, location_ptr);
 
-    *(int*)(location_ptr + heap->location_offset) = -1;
-    *(int*)(last_ptr + heap->location_offset) = location;
+    int* location_location = (int *)(location_ptr + heap->location_offset);
+    *location_location = INVALID_LOCATION;
+
+    int* last_location = (int*)(last_ptr + heap->location_offset);
+    *last_location = location;
 
     heap->data[location] = heap->data[heap->size - 1];
     heap->data[heap->size - 1] = location_ptr;
@@ -164,7 +153,7 @@ void* heap_remove_at(heap_t* heap, int location)
 
     if(heap->size < (heap->allocated_size / 2))
     {
-        heap->data = realloc(heap->data, (heap->allocated_size / 2) * heap->data_size);
+        heap->data = realloc(heap->data, (heap->allocated_size / 2) * sizeof(void*));
         heap->allocated_size /= 2;
     }
 
@@ -186,8 +175,6 @@ void heap_modify_element(heap_t* heap, void* location_ptr)
     void* parent_ptr = heap->data[PARENT(location)];
 
     int comp_val = heap->compare(location_ptr, parent_ptr);
-    printf("Comparison Val: %d Location: %d\n", comp_val, location);
-
     if (comp_val < 0)
     {
         heapify_up(heap, location);
@@ -223,20 +210,19 @@ void* heap_remove_top(heap_t* heap)
     void* start_ptr = heap->data[0];
     void* last_ptr = heap->data[heap->size - 1];
 
-    int* start_loc = (int *)(start_ptr + offset);
-    *start_loc = -1;
+    int* start_location = (int *)(start_ptr + offset);
+    *start_location = INVALID_LOCATION;
 
-    int* last_loc = (int *)(last_ptr + offset);
-    *last_loc = 0;
-
+    int* last_location = (int *)(last_ptr + offset);
+    *last_location = 0;
 
     heap->data[0] = heap->data[heap->size - 1];
-    heap->data[heap->size] = start_ptr;
+    heap->data[heap->size - 1] = start_ptr;
     heap->size--;
 
     if(heap->size < (heap->allocated_size / 2))
     {
-        heap->data = realloc(heap->data, (heap->allocated_size / 2) * heap->data_size);
+        heap->data = realloc(heap->data, (heap->allocated_size / 2) * sizeof(void*));
         heap->allocated_size /= 2;
     }
 
@@ -249,32 +235,20 @@ void* heap_remove_top(heap_t* heap)
 bool element_in_heap(heap_t* heap, void const* location_ptr)
 {
     if(heap->size == 0 || location_ptr == NULL)
-    {
-        printf("Input values for element_in_heap() invalid.\n");
         return false;
-    }
 
     int location = *(int *)(location_ptr + heap->location_offset);
 
-    return (location >=0 && location < heap->size);
+    return ((location >= 0) && (location < heap->size));
 }
 
 // If not using an associative mapping.
 int heap_find_location(heap_t* heap, void const* data)
 {
-    if(heap == NULL)
-    {
-        printf("Input values for heap_find_location() invalid.\n");
-        return -1;
-    }
-
-    int location = -1;
-
-    void const* location_ptr = NULL;
-
+    int location = INVALID_LOCATION;
     for(int i = 0; i < heap->size; i++)
     {
-        location_ptr = heap->data[i];
+        void const* location_ptr = heap->data[i];
         if(heap->find(location_ptr, data) == true)
         {
             location = i;
@@ -302,22 +276,28 @@ void print_heap(heap_t* heap)
         return;
     }
 
-    printf("\nHeap->size: %d\n", heap->size);
+    printf("Heap->size: %d\n", heap->size);
     for (int i = 0; i < heap->size; i++)
     {
         printf(" [%d]: ", i);
         heap->print(heap->data[i]);
-
     }
     printf("\n");
 
     return;
 }
 
+typedef struct _heap_data_t
+{
+    int key;
+    int heap_location;
+    char value[0];
+} heap_data_t;
+
 void print_heap_data(void const* ptr)
 {
     heap_data_t* data = (heap_data_t*) ptr;
-    printf("Key: %d Value: %s Heap_location: %d\n", data->key, data->value, data->heap_location);
+    printf("(%d, %s, %d)\n", data->key, data->value, data->heap_location);
 
     return;
 }
@@ -327,8 +307,6 @@ int min_heap_data(void const* left_ptr, void const* right_ptr)
     heap_data_t* left = (heap_data_t*) left_ptr;
     heap_data_t* right = (heap_data_t*) right_ptr;
 
-    printf("left->key: %d\n", left->key);
-    printf("right->key: %d\n", right->key);
     if(left->key < right->key)
     {
         return -1;
@@ -344,7 +322,10 @@ int min_heap_data(void const* left_ptr, void const* right_ptr)
 
 bool find_heap_data(void const* left_ptr, void const* right_ptr)
 {
-    return false;
+    heap_data_t* left = (heap_data_t*) left_ptr;
+    heap_data_t* right = (heap_data_t*) right_ptr;
+
+    return (left->key == right->key);
 }
 
 int main()
@@ -352,11 +333,10 @@ int main()
     heap_data_t** data = malloc(sizeof(heap_data_t*) * 10);
     for(int i = 0; i < 10; i++)
     {
-        data[i] = malloc(sizeof(heap_data_t));
+        data[i] = malloc(sizeof(heap_data_t) + sizeof(char) *  6);
         data[i]->key = i + 1;
         data[i]->heap_location = -1;
-        data[i]->value = malloc(sizeof(char) * 6);
-        data[i]->value = "Hello\n";
+        strcpy(data[i]->value, "Hello");
     }
 
     for(int i = 0; i < 10; i++)
@@ -366,7 +346,7 @@ int main()
 
     int location_offset = offsetof(heap_data_t, heap_location);
 
-    heap_t* min_heap = heap_init(sizeof(heap_data_t*), location_offset, min_heap_data, find_heap_data, print_heap_data);
+    heap_t* min_heap = heap_init(location_offset, min_heap_data, find_heap_data, print_heap_data);
 
     for(int i = 0; i < 10; i++)
     {
@@ -379,13 +359,21 @@ int main()
 
     print_heap(min_heap);
 
-    data[5]->value = "Thank\n";
+    strcpy(data[5]->value, "Thank");
     data[5]->key = 0;
+    print_heap(min_heap);
     heap_modify_element(min_heap, data[5]);
     print_heap(min_heap);
 
-    heap_remove_top(min_heap);
-    print_heap(min_heap);
+    for(int i = 0; i < 10; i++)
+    {
+        heap_data_t* top = heap_remove_top(min_heap);
+        print_heap(min_heap);
+        free(top);
+    }
+
+    free(data);
+    clear_heap(min_heap);
 
     return 1;
 }
--- Heap/pointer_heap.h
+++ Heap/pointer_heap.h
@@ -4,16 +4,15 @@ typedef struct _heap_t
 {
     int size;
     int allocated_size;
-    int data_size;
     int location_offset;
     int (*compare)(void const*, void const*);
     bool (*find)(void const*, void const*);
-    void (*print)(void const**);
+    void (*print)(void const*);
     void** data;
 } heap_t;
 
 // Constructor and destructor for heap.
-heap_t* heap_init(int data_size, int location_offset,
+heap_t* heap_init(int location_offset,
                   int (*compare)(void const*, void const*),
                   bool (*find)(void const*, void const*),
                   void (*print)(void const*));
@@ -22,7 +21,7 @@ void clear_heap(heap_t* heap);
 void print_heap(heap_t* heap);
 int get_heap_size(heap_t* heap); // Returns size of heap.
 
-void heap_insert(heap_t* heap, void const* data);
+void heap_insert(heap_t* heap, void* data);
 void* heap_remove_at(heap_t* heap, int location);
 
 void const* get_heap_top(heap_t* heap);
